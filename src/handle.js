47004708a7cff5238fc0f78e3bf848a82a89718d
francoode <francoode@gmail.com>
2021-05-13 21:35:45 -0300

    out img

--- src/handle.js
+++ src/handle.js
@@ -15,17 +15,31 @@ window.addEventListener('load', async () => {
 
 });
 
-document.getElementById('click').addEventListener('click', () => {
+document.getElementById('process').addEventListener('click', async () => {
+    try {
+        const db = await Database.getInstance();
+        const products = await db.getAllProducts();
 
-    Jimp.read(`${__dirname}/test.jpeg`)
-        .then((image) => {
-            Jimp.loadFont(Jimp.FONT_SANS_64_BLACK).then(font => {
-                image.print(font, 100, 500, 'hello');
-            }).then(() => {
-                image.write('jeje.jpeg');
-            })
-        })
-        .catch((e) => console.log(e));
+        if (products.length >= 1) {
+            for (const p of products) {
+                try {
+                    const path = `${__dirname}/img/${p.path}`;
+                    const image = await Jimp.read(path);
+                    const font = await Jimp.loadFont(Jimp.FONT_SANS_64_BLACK);
+                    const price = document.getElementById(`price-${p.id}`).value;
+                    await image.print(font, 100, 500, price);
+                    await image.write(`${__dirname}/out/${p.name}.jpg`);
+
+                    document.getElementById(`result-${p.id}`).innerHTML = `<span class="icon icon-check success"></span>`
+                } catch (e) {
+                    document.getElementById(`result-${p.id}`).innerHTML = `<span class="icon icon-cancel error"></span>`
+                    console.log(e);
+                }
+            }
+        }
+    } catch (e) {
+        console.log(e);
+    }
 });
 
 document.getElementById('form-new-product').addEventListener('submit', async (event) => {
@@ -34,7 +48,7 @@ document.getElementById('form-new-product').addEventListener('submit', async (ev
         const name = document.getElementById('product-name').value;
         const path = document.getElementById('product-file').files[0].name;
 
-        if(!name || !path) {
+        if (!name || !path) {
             throw new Error('Invalid value');
         }
 
@@ -42,13 +56,16 @@ document.getElementById('form-new-product').addEventListener('submit', async (ev
         const id = await db.insertProduct(name, path);
 
         insertRow({id, name, path});
-
-        document.getElementById("form-new-product").reset();
+        document.getElementById('form-new-product').reset();
+        document.getElementById('form-message-success').style.display = 'block';
+        document.getElementById('form-message-error').style.display = 'none';
 
         return false;
     } catch (e) {
-        console.log(e);
-        document.getElementById("form-new-product").reset();
+        document.getElementById('form-message-success').style.display = 'none';
+        document.getElementById('form-message-error').style.display = 'block';
+        document.getElementById('form-message-error').innerHTML = `<span class="icon icon-cancel"></span> ${e.toString()}`
+        document.getElementById('form-new-product').reset();
     }
 })
 
@@ -70,7 +87,7 @@ document.getElementById('tab-list').addEventListener('click', () => {
 
 function insertRow(product) {
     const tableRef = document.getElementById('products-list');
-    const newRow   = tableRef.insertRow(tableRef.rows.length);
+    const newRow = tableRef.insertRow(tableRef.rows.length);
 
     const cellName = newRow.insertCell();
     cellName.appendChild(document.createTextNode(product.name));
@@ -84,4 +101,11 @@ function insertRow(product) {
     priceElement.setAttribute('id', `price-${product.id}`);
     priceElement.setAttribute('required', 'true');
     cellPrice.appendChild(priceElement);
+
+    const cellResult = newRow.insertCell();
+    const resultElement = document.createElement('span');
+    resultElement.setAttribute('id', `result-${product.id}`);
+    resultElement.innerHTML = `<span id="delete-${product.id}" class="icon icon-trash"></span>`
+
+    cellResult.appendChild(resultElement);
 }
